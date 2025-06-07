c73d0243f2b4613b0c83560ec7ec8fd0d2bf91e0
VilCoder <176792549+VilCoder@users.noreply.github.com>
2025-06-07 11:39:55 -0500

    feat: add more tests to the calculator function

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,8 +1,33 @@
 export const calculator = (function () {
-  const add = (a, b) => a + b;
-  const subtract = (a, b) => a - b;
-  const multiply = (a, b) => a * b;
-  const divide = (a, b) => a / b;
+  const checkArguments = (a, b) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("Only numeric values are allowed");
+    }
+  };
+
+  const add = (a, b) => {
+    checkArguments(a, b);
+    return a + b;
+  };
+
+  const subtract = (a, b) => {
+    checkArguments(a, b);
+    return a - b;
+  };
+  const multiply = (a, b) => {
+    checkArguments(a, b)
+    return a * b;
+  }
+
+  const divide = (a, b) => {
+    checkArguments(a, b);
+
+    if (b === 0) {
+      throw new Error("Cannot be divided by 0");
+    }
+    
+    return a / b;
+  }
 
   return {
     add,
--- src/js/calculator.test.js
+++ src/js/calculator.test.js
@@ -1,18 +1,85 @@
 import { calculator } from "./calculator";
 
 /* eslint-disable no-undef */
-test("Adds 1 + 2 to equal", () => {
-  expect(calculator.add(1, 2)).toBe(3);
+describe("Functionality add test", () => {
+  test("Adds 1 + 2 to equal 3", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+  test("Works with errors", () => {
+    expect(() => calculator.add(1, "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.add("1", 23)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.add("two", 23)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.add("123", "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+  });
 });
 
-test("Subtract 2 - 1 to equal 1", () => {
-  expect(calculator.subtract(2, 1)).toBe(1);
+describe("Functionality subtract test", () => {
+  test("Subtract 2 - 1 to equal 1", () => {
+    expect(calculator.subtract(2, 1)).toBe(1);
+  });
+  test("Works with errors", () => {
+    expect(() => calculator.subtract(1, "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.subtract("1", 23)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.subtract("two", undefined)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.subtract("123", "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+  });
 });
 
-test("Multiply 2 * 2 to equal 4", () => {
-  expect(calculator.multiply(2, 2)).toBe(4);
+describe("Functionality multiply test", () => {
+  test("Multiply 2 * 2 to equal 4", () => {
+    expect(calculator.multiply(2, 2)).toBe(4);
+  });
+  test("Works with errors", () => {
+    expect(() => calculator.multiply(1, "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.multiply("1", 23)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.multiply("two", 8)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.multiply("123", "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+  });
 });
 
-test("Divide 4 / 2 to equal 2", () => {
-  expect(calculator.divide(4, 2)).toBe(2);
+describe("Functionality divide test", () => {
+  test("Divide 4 / 2 to equal 2", () => {
+    expect(calculator.divide(4, 2)).toBe(2);
+  });
+  test("Works with errors", () => {
+    expect(() => calculator.divide(1, "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.divide("1", 23)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.divide("two", 8)).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.divide("123", "23")).toThrow(
+      "Only numeric values are allowed",
+    );
+    expect(() => calculator.divide(123, 0)).toThrow(
+      "Cannot be divided by 0",
+    );
+  });
 });
